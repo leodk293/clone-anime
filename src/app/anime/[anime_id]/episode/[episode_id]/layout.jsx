0caad95e79fcc75a006e5f46fb71ee56753c460b
Aboubacar Traore <aboubatrao04@gmail.com>
2025-05-29 22:52:44 +0100

    episode title metadata added

--- src/app/anime/[anime_id]/episode/[episode_id]/layout.jsx
+++ src/app/anime/[anime_id]/episode/[episode_id]/layout.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
+export async function generateMetadata({ params }, parent) {
+  const episodeNumber = params.episode_id;
+  const animeId = params.anime_id;
+
+  const res = await fetch(
+    `https://api.jikan.moe/v4/anime/${animeId}/episodes/${episodeNumber}`
+  );
+  const result = await res.json();
+
+  if (result.data && result.data.title) {
+    return {
+      title: `${result.data.title} - Clone-Anime`,
+    };
+  }
+
+  return {
+    title: `Undefined - Clone-Anime`,
+  };
+}
+
 export default function layout({ children }) {
   return <>{children}</>;
 }
